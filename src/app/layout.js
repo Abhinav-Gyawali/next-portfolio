8ca617290486d0fb6a05f5da020f01af8f110979
Abhinav-Gyawali <95914404+Abhinav-Gyawali@users.noreply.github.com>
2024-09-10 17:51:16 +0545

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: "Abhinav G'",
   description: 'I&apos;m Abhinav Gyawali, a 16-year-old developer with a passion for coding and technology. I specialize in React and Django, and I love building innovative web solutions. I&apos;m always eager to learn, improve my skills, and take on new challenges in the tech world.',
-  image:'abhinav.jpg'
+  image:'IMG-20240717-WA0000.jpg'
 }
 
 export default function RootLayout({ children }) {
